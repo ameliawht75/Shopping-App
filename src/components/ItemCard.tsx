cfdeecb13be6cef9015c1649fde6ca40bf1d2ec0
ameliawht75 <ameliawht75@gmail.com>
2025-04-10 18:51:07 -0600

    upload 5

--- src/components/ItemCard.tsx
+++ src/components/ItemCard.tsx
@@ -1,4 +1,4 @@
-//Creates a card for each item that shows name, price, and quantity.  Added the delete and edit buttons here for simplicity. 
+//Creates a card for each item that shows name, price, and quantity.  Add the delete and edit buttons here for simplicity. 
 //Confirm delete and edit pop-up still needed.
 //If I can get the category idea to work will add that to the card as well depending on cosmetics.
 
@@ -7,12 +7,6 @@ export interface Item {
     name: string;
     price: number;
     quantity: number;
-  }
-  
-  interface Props {
-    item: Item;
-    deleteItem: (id: number) => void; //Delete function reciever
-    editItem: (item: Item) => void;  //Edit function reciever
   }
   
   function ItemCard({ item, deleteItem, editItem }: Props) {
